7b6c9f7ab6bdda6be622fa255b8604ba03f19999
Landon Stobaugh <landon.stobaugh@icloud.com>
2020-09-24 17:52:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ from scapy.all import sniff
 ## Create a Packet Counter
 packet_counts = Counter()
 
+#Variable that stores the captured packets
+captured_packets = []
+
 ## Define our Custom Action function
 def custom_action(packet):
     # Create tuple of Src/Dst in sorted order
-    print(type(packet[0][2]))
+    #print(type(packet[3]))
+    print(type(packet.lastlayer()))
     key = tuple(sorted([packet[0][1].src, packet[0][1].dst]))
     packet_counts.update([key])
-    return "Packet #{}: {} ==> {}".format(sum(packet_counts.values()), packet[0][1].src, packet[0][1].dst)
+
+    captured_packets.append(packet)
+    #return "Packet #{}: {} ==> {}".format(sum(packet_counts.values()), packet[0][1].src, packet[0][1].dst)
 
 ## Setup sniff, filtering for IP traffic
-sniff(filter="ip", prn=custom_action, count=10)
+sniff(prn=custom_action, count=100)
 
 ## Print out packet count per A <--> Z address pair
 print("\n".join(f"{f'{key[0]} <--> {key[1]}'}: {count}" for key, count in packet_counts.items()))
