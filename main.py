13406edd70cf92088863a3e1033fde28f7977353
Landon <landon.stobaugh@gmail.com>
2020-10-07 20:08:46 -0700

    Tabs 2 Spaces
    Maybe something I need to do in the final version

--- main.py
+++ main.py
@@ -12,31 +12,31 @@ black_list = ["Raw", "TCP", "UDP"]
 
 ## Function that handles packets that might be clear text
 def handle_cleartext(pkt):
-	global captured_packets
-	
-	lyr = pkt.lastlayer()
-	
-	# Check if we don't want this kind of packet
-	if lyr.name in black_list:
-		# Returns false if we don't want this kind of packet
-		return False
-	
-	# Add packet to "what we want" list
-	captured_packets.append(pkt)
-	
-	# Return true for a success
-	return True
-	
+    global captured_packets
+    
+    lyr = pkt.lastlayer()
+    
+    # Check if we don't want this kind of packet
+    if lyr.name in black_list:
+        # Returns false if we don't want this kind of packet
+        return False
+    
+    # Add packet to "what we want" list
+    captured_packets.append(pkt)
+    
+    # Return true for a success
+    return True
+    
 
 ## Define our Custom Action function
 def custom_action(packet):
 
     # Handle if packet is cleartext
     if handle_cleartext(packet):
-		try:
-			return "Packet #{}: {} ==> {} Type: {}".format(sum(packet_counts.values()), packet[0][1].src, packet[0][1].dst, packet.lastlayer().name)
-		except:
-			return ""
+        try:
+            return "Packet #{}: {} ==> {} Type: {}".format(sum(packet_counts.values()), packet[0][1].src, packet[0][1].dst, packet.lastlayer().name)
+        except:
+            return packet.lastlayer().name
 
 ## Setup sniff, filtering for IP traffic
 pkts = sniff(prn=custom_action)
