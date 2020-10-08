ffade500ead5358fa3efbc64ba74ab1fb4b1d654
Landon <landon.stobaugh@gmail.com>
2020-10-07 19:56:09 -0700

    Clean up!
    Cleaned the code a tad

--- main.py
+++ main.py
@@ -18,28 +18,26 @@ def handle_cleartext(pkt):
 	
 	# Check if we don't want this kind of packet
 	if lyr.name in black_list:
+		# Returns false if we don't want this kind of packet
 		return False
-		
-	print(lyr.name)
+	
+	# Add packet to "what we want" list
 	captured_packets.append(pkt)
+	
+	# Return true for a success
 	return True
 	
 
 ## Define our Custom Action function
 def custom_action(packet):
-    # Create tuple of Src/Dst in sorted order
-    #print(type(packet[3]))
-    #print(packet.show())
-    #key = tuple(sorted([packet[0][1].src, packet[0][1].dst]))
-    #packet_counts.update([key])
-    
+
     # Handle if packet is cleartext
     if handle_cleartext(packet):
-		return "Packet #{}: {} ==> {}".format(sum(packet_counts.values()), packet[0][1].src, packet[0][1].dst)
+		try:
+			return "Packet #{}: {} ==> {}".format(sum(packet_counts.values()), packet[0][1].src, packet[0][1].dst)
+		except:
+			return ""
 
 ## Setup sniff, filtering for IP traffic
 pkts = sniff(prn=custom_action)
 wrpcap('foo.pcap', captured_packets)
-
-## Print out packet count per A <--> Z address pair
-#print("\n".join(f"{f'{key[0]} <--> {key[1]}'}: {count}" for key, count in packet_counts.items()))
