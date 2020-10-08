60c243e6b006feed8a61006214df7ad3d4cf8d7e
Landon <landon.stobaugh@gmail.com>
2020-10-07 19:39:27 -0700

    Quick Add
    A little different

--- main.py
+++ main.py
@@ -8,6 +8,22 @@ packet_counts = Counter()
 
 captured_packets = []
 
+black_list = ["Raw", "TCP", "UDP"]
+
+## Function that handles packets that might be clear text
+def handle_cleartext(pkt):
+	global captured_packets
+	
+	lyr = pkt.lastlayer()
+	
+	# Check if we don't want this kind of packet
+	if lyr.name in black_list:
+		return
+		
+	print(last_layer.name)
+	captured_packets.append(pkt)
+	
+
 ## Define our Custom Action function
 def custom_action(packet):
     # Create tuple of Src/Dst in sorted order
@@ -15,14 +31,16 @@ def custom_action(packet):
     #print(packet.show())
     #key = tuple(sorted([packet[0][1].src, packet[0][1].dst]))
     #packet_counts.update([key])
+    
+    # Handle if packet is cleartext
+    handle_cleartext(packet)
 
 
-    captured_packets.append(packet)
-    #return "Packet #{}: {} ==> {}".format(sum(packet_counts.values()), packet[0][1].src, packet[0][1].dst)
+    return "Packet #{}: {} ==> {}".format(sum(packet_counts.values()), packet[0][1].src, packet[0][1].dst)
 
 ## Setup sniff, filtering for IP traffic
 pkts = sniff(prn=custom_action)
-wrpcap('foo.pcap', pkts)
+wrpcap('foo.pcap', captured_packets)
 
 ## Print out packet count per A <--> Z address pair
-print("\n".join(f"{f'{key[0]} <--> {key[1]}'}: {count}" for key, count in packet_counts.items()))
+#print("\n".join(f"{f'{key[0]} <--> {key[1]}'}: {count}" for key, count in packet_counts.items()))
