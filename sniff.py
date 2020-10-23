26f3f6f5944dfe4be8f313c7f703b0cca075b736
Landon <landon.stobaugh@gmail.com>
2020-10-23 15:53:54 -0700

    edits
    edits

--- sniff.py
+++ sniff.py
@@ -16,8 +16,23 @@ class Sniffer:
 	
 	def init_values(self):
 		# These are the default settings of the program
+		
 		# IPv4
 		self.sniff_values[self.sniff_attributes[0]] = True
+		
+		# IPv6
+		self.sniff_values[self.sniff_attributes[1]] = True
+		
+		# MAC
+		self.sniff_values[self.sniff_attributes[2]] = True
+		
+		# FilterIP
+		self.sniff_values[self.sniff_attributes[3]] = None
+		
+	
+	def validate_instruction(self, sniff_att, sniff_val):
+		if type(self.sniff_values[sniff_att]):
+			pass
 	
 	def validate_format(self, inp_format):
 		
@@ -31,6 +46,12 @@ class Sniffer:
 			instruction_parts = instruction.split(self.value_seperator)
 			
 			if not len(intruction_parts) == 2:
+				return False
+			
+			if not instruction_parts[0] in self.sniff_attributes:
+				return False
+			
+			if not validate_instruction(instruction_parts[0], instruction_parts[1]):
 				return False
 			
 	
