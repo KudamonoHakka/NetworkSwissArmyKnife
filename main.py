5288d6af999743e5a846aa5f8f61681c5f8a0ab4
Landon <landon.stobaugh@gmail.com>
2020-10-23 14:01:23 -0700

    GUI update
    title

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import sys
 from scapy.all import *
 
 ## Create a Packet Counter
@@ -8,7 +9,10 @@ packet_counts = Counter()
 
 captured_packets = []
 
-black_list = ["Raw", "TCP", "UDP"]
+# Default list of what not to include
+black_list = ["Raw", "TCP", "UDP", "ARP"]
+
+
 
 ## Function that handles packets that might be clear text
 def handle_cleartext(pkt):
@@ -38,6 +42,98 @@ def custom_action(packet):
         except:
             return packet.lastlayer().name
 
+def printHeader():
+	## The logo of the application.
+	print(" _   _  _   ___ _  ")
+	print("| \ | || | / (_) |  ")
+	print("|  \| || |/ / _| |_ ")
+	print("| . ` ||    \| | __|")
+	print("| |\  || |\  \ | |_ ")
+	print("\_| \_/\_| \_/_|\__|")
+	print("The general purpose network hacking tool")
+
+def printOptions():
+	# Options menu for the main menu
+	print("1) Packet Sniffer")
+	print("2) ARP Spoof")
+	print("q: Quit")
+
+# Parameters for packet sniffing
+packet_sniff_filename = "foo"
+packet_sniff_count = 0
+packet_sniff_filter = "default"
+
+def printPacketSniffOptions():
+	# Menu for packet sniffer
+	print("")
+	print("+=+=+ Packet Sniffer +=+=+")
+	print("")
+	print("___Parameters___")
+	print("file_name: {}".format(packet_sniff_filename))
+	print("packet_cap (0 for infinite): {}".format(packet_sniff_count))
+	print("filter: {}".format(packet_sniff_filter))
+	print("")
+	print("___Commands___")
+	print("set [variable] [value]) Change parameter value")
+	print("op) Options (this menu)")
+	print("r) Run")
+	print("b) Back")
+
+
+
+run_program = True
+
+def handleMenu():
+	# Print logo of project
+	printHeader()
+	
+	while run_program:
+		# Print the different tools program offers
+		printOptions()
+		
+		# Get user input
+		inp_cmd = raw_input(": ")
+		
+		# Quit application if user specifies
+		if inp_cmd == "q":
+			sys.exit(0)
+		
+		# Packet Sniffer Menu
+		elif str(inp_cmd) == "1":
+			
+			# Print different parameters for the packet sniffer tool
+			printPacketSniffOptions()
+			
+			# Variable for staying in sniffer menu
+			menu_sniff_run = True
+			
+			# Keep user in sniffer menu until specified
+			while menu_sniff_run:
+				
+				# Get user input
+				inp_cmd = raw_input(": ")
+				
+				# List options
+				if inp_cmd == "op":
+					printPacketSniffOptions()
+				
+				# Exit this tool
+				elif inp_cmd == "b":
+					menu_sniff_run = False
+					continue
+				
+				# Execute tool with parameters
+				elif inp_cmd == "r":
+					pass
+				
+				# Something wrong with specified command
+				else:
+					print("Unknown Command '{}'".format(inp_cmd))
+		else:
+			print("Unknown command")
+		
+
+handleMenu()
 ## Setup sniff, filtering for IP traffic
 pkts = sniff(prn=custom_action)
 wrpcap('foo.pcap', captured_packets)
