d3bf94b1a0698926ff956bdc0377aaf45e777f7e
Kudamono <71854345+KudamonoHakka@users.noreply.github.com>
2021-07-18 17:23:28 -0700

    Sniff fix and new mod start

--- sniff_count.py
+++ sniff_count.py
@@ -2,43 +2,34 @@ import sys
 import os
 from scapy.all import *
 
-class Sniffer:
+class Sniffer_Coung:
     def __init__(self):
-        self.options = {"file_name":"foo", "packet_count":0, "packet_filter":"default", "interface":"eth0"}
+        self.data = {"packet_count":0, "start_time":None}
+        self.options = {"packet_count":20, "packet_filter":"default", "interface":"eth0"}
     def set_property(self, item_prop, item_val):
         if item_prop in self.options:
             self.options[item_prop] = item_val
         else:
             print("Not a valid option")
     def sniff_net(self):
-        pkts = None
+        
         if self.options["packet_filter"] == "default":
             try:
-                if int(self.options["packet_count"]) == 0:
-                    pkts = sniff(filter="not arp and not icmp", iface=self.options["interface"])
-                else:
-                    pkts = sniff(filter="not arp and not icmp", count=int(self.options["packet_count"]), iface=self.options["interface"])
-
+                pkts = sniff(filter="not arp and not icmp", count=int(self.options["packet_count"]), iface=self.options["interface"])
             except:
                 print("Hmm... Something went wrong. Make sure this program is running as sudo and none of the settings are invalid.")
         else:
             try:
-                if int(self.options["packet_count"]) == 0:
-                    pkts = sniff(filter=self.options["packet_filter"], iface=self.options["interface"])
-                else:
-                    pkts = sniff(filter=self.options["packet_filter"], count=int(self.options["packet_count"]), iface=self.options["interface"])
-
+                pkts = sniff(filter=self.options["packet_filter"], count=int(self.options["packet_count"]), iface=self.options["interface"])
             except:
                 print("Hmm... Something went wrong. Make sure this program is running as sudo and none of the settings are invalid.")
-        wrpcap(self.options["file_name"]+'.pcap', pkts)
 
     def printOptions(self):
         print("")
         print("+=+=+ Packet Sniffer +=+=+")
         print("")
         print("___Parameters___")
-        print("file_name: {}".format(self.options["file_name"]))
-        print("packet_count (0 for infinite): {}".format(self.options["packet_count"]))
+        print("packet_cap (packets to collect before calculating time delta): {}".format(self.options["packet_count"]))
         print("filter: {}".format(self.options["packet_filter"]))
         print("interface: {}".format(self.options["interface"]))
         print("")
