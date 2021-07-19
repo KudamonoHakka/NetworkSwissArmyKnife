ce468f7afbe6486fd118f5dcc6b5e726fe915537
Kudamono <71854345+KudamonoHakka@users.noreply.github.com>
2021-07-18 22:31:02 -0700

    MAJOR BUG FIXES
    Couple of embarrassing mistakes here and there, but it got taken care of 笑笑

--- sniff_count.py
+++ sniff_count.py
@@ -1,49 +1,37 @@
 import sys
 import os
+import time
 from scapy.all import *
 
-class Sniffer:
+class Sniffer_Count:
     def __init__(self):
-        self.options = {"file_path": "", "file_name":"foo", "packet_count":0, "packet_filter":"default"}
+        self.data = {"packet_count":0, "start_time":None}
+        self.options = {"packet_count":20, "packet_filter":"default"}
     def set_property(self, item_prop, item_val):
         if item_prop in self.options:
             self.options[item_prop] = item_val
         else:
             print("Not a valid option")
     def sniff_net(self):
+        self.data["start_time"] = time.time()
         pkts = None
         if self.options["packet_filter"] == "default":
             try:
-                if int(self.options["packet_count"]) == 0:
-                    #pkts = sniff(filter="not arp and not icmp", iface=self.options["interface"])
-                    pkts = sniff(filter="not arp and not icmp")
-                else:
-                    #pkts = sniff(filter="not arp and not icmp", count=int(self.options["packet_count"]), iface=self.options["interface"])
-                    pkts = sniff(filter="not arp and not icmp", count=int(self.options["packet_count"]))
+                pkts = sniff(filter="not arp and not icmp", count=int(self.options["packet_count"]))
             except:
                 print("Hmm... Something went wrong. Make sure this program is running as sudo and none of the settings are invalid. If you're using windows, make sure you have npcap installed")
         else:
             try:
-                if int(self.options["packet_count"]) == 0:
-                    pkts = sniff(filter=self.options["packet_filter"])
-                else:
-                    pkts = sniff(filter=self.options["packet_filter"], count=int(self.options["packet_count"]))
-
+                pkts = sniff(filter=self.options["packet_filter"], count=int(self.options["packet_count"]))
             except:
                 print("Hmm... Something went wrong. Make sure this program is running as sudo and none of the settings are invalid.")
-        path_ = self.options["file_path"]+self.options["file_name"]+'.pcap'
-        path_ = path_[1:]
-        pktdump = PcapWriter(path_, append=False, sync=True)
-        pktdump.write(pkts)
 
     def printOptions(self):
         print("")
         print("+=+=+ Packet Sniffer +=+=+")
         print("")
         print("___Parameters___")
-        print("file_path (PLEASE CHANGE!: Example: C:\\Somewhere\\NotSure\\): {}".format(self.options["file_path"]))
-        print("file_name: {}".format(self.options["file_name"]))
-        print("packet_count (0 for infinite): {}".format(self.options["packet_count"]))
+        print("packet_count (packets to collect before calculating time delta): {}".format(self.options["packet_count"]))
         print("filter: {}".format(self.options["packet_filter"]))
         print("")
         print("___Commands___")
@@ -65,6 +53,7 @@ class Sniffer:
             self.printOptions()
         elif spaced_cmd[0] == "r":
             self.sniff_net()
+            print("Total time: "+str(time.time()-self.data["start_time"]) + " seconds")
         elif spaced_cmd[0] == "b":
             return 0
         elif spaced_cmd[0] == "set" and len(spaced_cmd) >= 2:
